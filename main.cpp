321758de0e0a6c632e753cd1962e3971e9e96c7b
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-12 10:36:05 +0300

    add fib_seq

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
+
+unsigned long fib_seq(int n) {
+    unsigned long old = 0;
+    unsigned long value = 1;
+    unsigned long hold;
+
+    if(n < 1) return 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        hold = value;
+        value += old;
+        old = hold;
+    }
+
+    return value;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << fib_seq(45);
     return 0;
 }
