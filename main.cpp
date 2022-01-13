b62a2cd69d2a501f26df7abc5620f1c4d218c8d6
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-13 12:35:35 +0300

    Add Some OMP functions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <omp.h>
+
+#define ll unsigned long long
+
 using namespace std;
 
-unsigned long fib_seq(int n) {
-    unsigned long old = 0;
-    unsigned long value = 1;
-    unsigned long hold;
+ll fib(int n) {
+    if (n < 2) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+ll fib_seq(int n) {
+    ll old = 0;
+    ll value = 1;
+    ll hold;
 
     if(n < 1) return 0;
 
-    for(int i = 1; i < n; i++)
+    for(ll i = 1; i < n; i++)
     {
         hold = value;
         value += old;
@@ -18,7 +27,61 @@ unsigned long fib_seq(int n) {
     return value;
 }
 
+ll fib_omp_sections(int n) {
+    ll x, y;
+    #pragma omp parallel
+    {
+        #pragma omp parallel sections shared(x, y)
+        {
+            #pragma omp section
+            {
+                x = fib(n - 1);
+            }
+            #pragma omp section
+            {
+                y = fib(n - 2);
+            }
+        }
+    }
+
+    return x + y;
+}
+
+ll fib_omp_tasks(int n) {
+    ll x, y;
+    if (n < 2) return n;
+    #pragma omp parallel shared(n)
+    {
+        #pragma omp single
+        {
+            #pragma omp task shared(x)
+            x = fib_omp_tasks(n - 1);
+
+            #pragma omp task shared(y)
+            y = fib_omp_tasks(n - 2);
+
+            #pragma omp taskwait
+        }
+    }
+
+    return x + y;
+}
+
+
+void run(const string& name, ll f(int)) {
+    double time = omp_get_wtime();
+    ll res = f(50);
+    printf("%s Time: %f Result: %llu\n", name.c_str() , omp_get_wtime() - time, res);
+}
+
+
 int main() {
-    cout << fib_seq(45);
+    omp_set_num_threads(4);
+
+    run("Recursive", fib);
+    run("Sequence ", fib_seq);
+    run("Sections ", fib_omp_sections);
+    run("Tasks    ", fib_omp_tasks);
+
     return 0;
 }
