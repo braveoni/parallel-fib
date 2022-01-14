2d6dbcd6cdbe762fd34af6269c87832441b8f272
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-14 05:55:06 +0300

    Add baseline solution

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define ull unsigned long long
-#define N 15
+#define N 100000
 
 ull value[N+1];
 bool done[N+1];
@@ -11,19 +11,53 @@ omp_lock_t lock[N+1];
 
 const ull e = 10e10 + 7;
 
-ull fib(int n) {
+ull fib_seq(ull n) {
+    ull old = 0;
+    ull result = 1;
+    ull hold;
+
+    if (n < 1) return 0;
+    for (int i = 0; i < n; ++i) {
+        hold = result;
+        result = (result + old) % e;
+        old = hold;
+    }
+
+    return result;
+}
+
+ull fib_omp(ull n) {
+    if (n < 2) return n;
+
+    ull r1, r2;
+
+#pragma omp task shared(r1)
+    {
+        r1 = fib_omp(n - 1);
+    }
+
+#pragma omp task shared(r2)
+    {
+        r2 = fib_omp(n - 2);
+    }
+
+#pragma omp taskwait
+    return r1 + r2;
+}
+
+ull fib_omp_bor(ull n) {
     ull i, j;
 
     omp_set_lock( &(lock[n]) );
 
     if (!done[n]) {
-        #pragma omp parallel shared(value, done)
+        #pragma omp parallel
         {
             #pragma omp task shared(i) firstprivate(n)
-            i = fib(n - 1);
+            i = fib_omp_bor(n - 1);
 
             #pragma omp task shared(j) firstprivate(n)
-            j = fib(n - 2);
+            j = fib_omp_bor(n - 2);
 
             #pragma omp taskwait
             value[n] = (i + j) % e;
@@ -35,8 +69,17 @@ ull fib(int n) {
     return value[n];
 }
 
+void run(ull f(ull)) {
+    double time = omp_get_wtime();
+    ull res = f(N);
+    printf("Time: %f Result: %llu \n", omp_get_wtime() - time, res);
+}
+
 int main() {
-    value[0] = 0;
+
+    run(fib_omp);
+
+    value[0] = 1;
     value[1] = 1;
 
     for (int i = 0; i <= N; i++) done[i] = false;
@@ -45,6 +88,6 @@ int main() {
     done[1] = true;
 
     for (int i=0; i <= N; i++) omp_init_lock(&(lock[i]));
-    cout << fib(N) << '\n';
+    run(fib_omp_bor);
     for (int i=0; i <= N ; i++) omp_destroy_lock(&(lock[i]));
 }
